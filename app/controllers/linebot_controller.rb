55534ce741a28d52b5ce6f84f41e6da0e51a613c
tmrekk121 <tmrekk121@outlook.jp>
2020-05-23 21:29:10 +0900

    Webhook URL Sample finish

---
+++ app/controllers/linebot_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class LinebotController < ApplicationController
+  require 'line/bot'
+
+  def client
+    @client ||= Line::Bot::Client.new do |config|
+      config.channel_id = ENV['LINE_CHANNEL_ID']
+      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+    end
+  end
+
+  def callback
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    head :bad_request unless client.validate_signature(body, signature)
+
+    head :ok
+  end
+end
