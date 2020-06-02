ea0ff2c37fc07dfc4daa8abb0b43599294da6a26
tmrekk121 <tmrekk121@outlook.jp>
2020-06-03 00:01:27 +0900

    Add Delete Button

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -19,6 +19,8 @@ class LinebotController < ApplicationController
     events = client.parse_events_from(body)
     events.each do |event|
       case event
+      when Line::Bot::Event::Postback
+        logger.debug(event)
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
